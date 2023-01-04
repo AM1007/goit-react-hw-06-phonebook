0c89942f7c292e438c812493231a15550938b46e
AM1007 <andrew.motko@gmail.com>
2023-01-04 06:50:48 +0200

    data restored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
+import { useDispatch, useSelector } from 'react-redux';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import css from './App.module.css';
+
+import { addContact, remove } from 'redux/slices/contactSlice';
+import { contactFilter } from 'redux/slices/filterSlice';
+
+export default function App() {
+  const contacts = useSelector(state => state.contacts.contacts);
+  const filter = useSelector(state => state.filter.filter);
+  const dispatch = useDispatch();
+
+  const addContacts = ({ id, name, number }) => {
+    if (
+      contacts.find(contact => {
+        return contact.name === name;
+      })
+    ) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    const contact = {
+      id,
+      name,
+      number,
+    };
+    dispatch(addContact(contact));
+  };
+
+  const filterChange = e => {
+    dispatch(contactFilter(e.currentTarget.value));
+  };
+
+  const filterContact = contacts.filter(contact => {
+    return contact.name.toLowerCase().includes(filter.toLowerCase());
+  });
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm addContacts={addContacts} />
+      <h2>Contacts</h2>
+      <Filter filter={filterChange} />
+      <ContactList filter={filterContact} onDeleteContact={remove} />
     </div>
   );
-};
+}
